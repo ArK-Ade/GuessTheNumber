492c6e39543aee7afb5bddeca05b494366bd2b68
Anthony ADELAIDE <anthony.adelaide@orange.fr>
2022-02-01 15:20:42 +0100

    cleaning

--- app/src/main/java/com/example/guessthenumber/MainActivity.java
+++ app/src/main/java/com/example/guessthenumber/MainActivity.java
@@ -43,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 finish();
                 startActivity(getIntent());
-
             }
         });
 
