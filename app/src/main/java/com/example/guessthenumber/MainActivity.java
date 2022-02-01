7127ba42aeee330732b36159028491816f2579f2
Anthony ADELAIDE <anthony.adelaide@orange.fr>
2022-02-01 15:09:04 +0100

    Fonction recommencer et quitter

--- app/src/main/java/com/example/guessthenumber/MainActivity.java
+++ app/src/main/java/com/example/guessthenumber/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.guessthenumber;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
 
@@ -19,24 +18,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Random r = new Random();
-        int i1 = r.nextInt(100 - 0);
+        int i1 = r.nextInt(100);
 
-        ConstraintLayout Layout =  (ConstraintLayout) findViewById(R.id.layout);
-        TextView viewText = (TextView) this.findViewById(R.id.texte);
-        EditText edit = (EditText) this.findViewById(R.id.Resultat);
-        Button button = (Button) this.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener(){
+        //ConstraintLayout Layout =  (ConstraintLayout) findViewById(R.id.layout);
+        TextView viewText = this.findViewById(R.id.texte);
+        EditText edit = this.findViewById(R.id.Resultat);
+        Button button = this.findViewById(R.id.button);
+        Button recommencer = this.findViewById(R.id.button2);
+        Button quitter = this.findViewById(R.id.button3);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 //button.setText(String.valueOf(i1));
-                String choice = (String) edit.getText().toString();
-                Check(i1,Integer.parseInt(choice),viewText);
+                String choice = edit.getText().toString();
+                Check(i1, Integer.parseInt(choice), viewText, recommencer, quitter);
             }
         });
+
+        recommencer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                startActivity(getIntent());
+
+            }
+
+        });
+
+        quitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 
-    public void Check(int random, int choice, TextView edit){
+    public void Check(int random, int choice, TextView edit, Button recommencer, Button quitter){
         if(choice > random)
         {
             edit.setText(R.string.greater);
@@ -44,6 +65,8 @@ public class MainActivity extends AppCompatActivity {
             edit.setText(R.string.lesser);
         }else{
             edit.setText(R.string.goodNumber);
+            recommencer.setVisibility(View.VISIBLE);
+            quitter.setVisibility(View.VISIBLE);
         }
     }
 }
